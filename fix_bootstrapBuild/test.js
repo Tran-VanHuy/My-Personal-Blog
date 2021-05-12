9000f9ca418ab2ab02b25a1fe028f4e10fb5bd6e
X7md <54203033+X7md@users.noreply.github.com>
2021-05-12 09:15:02 +0300

    Try with cloudflarePages! 2

--- fix_bootstrapBuild/test.js
+++ fix_bootstrapBuild/test.js
@@ -1,10 +1,12 @@
-let fs = require('fs')
+let fs = require('fs');
+let path = require('path');
 
-fs.unlink('./node_modules/bootstrap/js/dist/collapse.js',(err) => {
+fs.unlink(path.join(__dirname,'../node_modules/bootstrap/js/dist/collapse.js'),(err) => {
     if (err) throw err;
     //console.log('successfully deleted /tmp/hello');
-    fs.copyFile('./fix_bootstrapBuild/collapse.js', './node_modules/bootstrap/js/dist/collapse.js', (err) => {
-        /*if (err) throw err;
-        console.log('Rename complete!');*/
-      });
+    fs.copyFile(path.join(__dirname, '/collapse.js'), path.join(__dirname, '../node_modules/bootstrap/js/dist/collapse.js'), (err) => {
+        if (err) throw err;
+        //console.log('Rename complete!');
+     });
   });
+//console.log(__dirname);
