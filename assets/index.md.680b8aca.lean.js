f3fc34e71616fa8fc6456cb14923d628a3529708
X7md <54203033+X7md@users.noreply.github.com>
2021-05-12 09:40:29 +0300

    Updates

---
+++ assets/index.md.680b8aca.lean.js
@@ -0,0 +1,1 @@
+import{c as t,a,F as e,r,o as n,t as l}from"./app.44476a74.js";const o='{"title":"Home","description":"","frontmatter":{"home":true,"lang":"ar"},"relativePath":"index.md","lastUpdated":1620795308648}',s={};s.render=function(o,s,i,d,p,u){return n(),t("div",null,[a("div",null,[(n(),t(e,null,r(3,(t=>a("p",null," Testing "+l(t),1))),64))])])};export default s;export{o as __pageData};
